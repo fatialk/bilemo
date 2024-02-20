4f869dc30d85f269657d19b196f2088d25417b37
utilisateur <elachri.fz@gmail.com>
2024-02-20 15:26:48 +0100

    Set up authorizations

--- src/Controller/ClientsController.php
+++ src/Controller/ClientsController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Repository\ClientRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\Client;
 
 #[Route('/clients')]
 class ClientsController extends AbstractController
 {
     #[Route('/', name: 'app_clients', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN', message: 'You don\'t have rights to access this page')]
     public function getClientsList(ClientRepository $clientRepository, SerializerInterface $serializer): JsonResponse
     {
         $clientsList = $clientRepository->findAll();
